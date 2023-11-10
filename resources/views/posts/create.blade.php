8e1e19bd9a5ce9e1fd90a4f1547f9ac9a7943ace
Kaisyu1223 <yamadakaisyu1223@gmail.com>
2023-11-10 20:50:12 +0900

    createとstore

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,12 +13,12 @@
             @csrf
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name=post[title] paginate="タイトル">
+                <input type="text" name=post[title] placeholder="タイトル">
             </div>
             <div class="body">
                 <h2>Body</h2>
                 <textarea name="post[body]" placeholder="今日も一日お疲れ様でした。"></textarea>
-            </div
+            </div>
             <input type="submit" value="store">
         </form>
         <div class='footer'>
